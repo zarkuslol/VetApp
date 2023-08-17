7715e3c35234087c9a10ba39a53669f3338945f2
zarkuslol <lucasferlima8@gmail.com>
2023-08-17 13:36:19 -0300

    Initial commit, Docker configured, front, back and DB working

---
+++ VetAppBack/src/main/java/atlas/VetApp/VetAppApplication.java
@@ -0,0 +1,13 @@
+package atlas.VetApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetAppApplication.class, args);
+	}
+
+}
